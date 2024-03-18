97d682312070bf2fb318377c0fdb87ba4a8b9284
Dione Ousseynou <dioneousseynou03@gmail.com>
2024-03-18 16:09:53 +0000

    update e2e test

--- script_indatacore.js
+++ script_indatacore.js
@@ -13,7 +13,7 @@ export const options = {
   scenarios: {
     sasl_auth: {
       executor: "constant-vus",
-      vus: 5,   // Nombre de utilisateurs virtuels
+      vus: 5,   // Nombre d'utilisateurs virtuels
       duration: "5s",  // Durée du test de charge
     //  gracefulStop: "5s",
     },
@@ -35,19 +35,29 @@ const saslConfig = {
 const offset = 0;
 const partition = 0;
 
-// Initialisation du Writer et du Reader
-const writer = new Writer({
+// Initialisation du Writer et du Reader pour Display-line-Balance-producer-topic
+const producerWriter = new Writer({
   brokers: brokers,
   topic: producerTopic,
   sasl: saslConfig,
 });
-const reader = new Reader({
+const producerReader = new Reader({
+  brokers: brokers,
+  topic: producerTopic,
+  partition: partition,
+  offset: offset,
+  sasl: saslConfig,
+});
+
+// Initialisation du Reader pour Display-line-Balance-consumer-topic
+const consumerReader = new Reader({
   brokers: brokers,
   topic: consumerTopic,
   partition: partition,
   offset: offset,
   sasl: saslConfig,
 });
+
 const connection = new Connection({
   address: brokers[0],
   sasl: saslConfig,
@@ -62,7 +72,7 @@ export const options1 = {
 };
 
 export default function () {
-  // Production de messages vers le topic producteur
+  // Production de messages vers le topic producteur Display-line-Balance-producer-topic
   for (let index = 0; index < 5; index++) {
     let messages = [
       {
@@ -85,69 +95,52 @@ export default function () {
       },
     ];
 
-    writer.produce({ messages: messages });
+    producerWriter.produce({ messages: messages });
   }
 
   // Attendre un court instant pour que les messages soient disponibles pour la consommation
   sleep(1);
 
-  // Consommation de messages du topic consommateur
-  let messagesConsumer = reader.consume({ limit: 10 });
-  check(messagesConsumer, {
-    "at least one message returned": (msgs) => msgs.length > 0,
+  // Consommation de messages du topic consommateur Display-line-Balance-consumer-topic
+  let consumerMessages = consumerReader.consume({ limit: 10 });
+  check(consumerMessages, {
+    "at least one message returned from consumer topic": (msgs) => msgs.length > 0,
+  });
+
+  // Consommation de messages du topic producteur Display-line-Balance-producer-topic
+  let producerMessages = producerReader.consume({ limit: 10 });
+  check(producerMessages, {
+    "at least one message returned from producer topic": (msgs) => msgs.length > 0,
   });
 
   // Vérification que les messages proviennent du topic producteur
-  for (let msg of messagesConsumer) {
-    check(msg.topic === consumerTopic, {
-      "message is from producer topic": () => msg.topic === consumerTopic,
+  for (let msg of producerMessages) {
+    check(msg.topic === producerTopic, {
+      "message is from producer topic": () => msg.topic === producerTopic,
     });
   }
 
-  // Vérification de la structure et du contenu des messages
-  for (let msg of messagesConsumer) {
+  /*
+  // Vérification de la structure et du contenu des messages consommés
+  for (let msg of consumerMessages) {
     const deserialized = schemaRegistry.deserialize({
       data: msg.value,
       schemaType: SCHEMA_TYPE_JSON,
     });
+  } */
 
-    check(deserialized, {
-      "message has expected structure": () => deserialized !== null && deserialized !== undefined,
-      "value is correct": () => {
-        return deserialized.IBSubscriber.ib_mdn === "123456" &&
-               deserialized.IBSubscriber.ib_level === "1" &&
-               deserialized.IBSubscriber.ib_levelRetireTime === "2023-06-27T16:46:47" &&
-               deserialized.IBSubscriber.ib_sublevel === "" &&
-               deserialized.IBOperation.origin === "CVM" &&
-               deserialized.IBOperation.user === "CVM-SYS" &&
-               deserialized.IBOperation.uuid === "1687798007798431503";
-      }
+ /* for (let msg of producerMessages) {
+    const deserialized = schemaRegistry.deserialize({
+      data: msg.value,
+      schemaType: SCHEMA_TYPE_JSON,
     });
-
-    // Ajouter des étiquettes aux métriques pour distinguer les topics
-    const topicLabels = { topic: consumerTopic }; // Étiquettes pour le topic consommateur
-    const topicProducerLabels = { topic: producerTopic }; // Étiquettes pour le topic producteur
-
-    // Enregistrer les métriques avec les étiquettes appropriées
-    // Par exemple, pour le nombre de messages consommés
-    const metricName = 'kafka_consumer_messages_count';
-    const metricValue = 1; // Nombre de messages consommés
-    const metricTags = topicLabels; // Utilisation des étiquettes du topic consommateur
-    const metric = {}; // Définition de la métrique
-    metric[metricName] = metricValue; // Définition de la valeur de la métrique
-    // Émission de la métrique avec les étiquettes spécifiées
-    check(metric, metricTags);
-
-    // Vous pouvez faire de même pour d'autres métriques que vous souhaitez enregistrer
-    // Assurez-vous de définir correctement les noms et valeurs des métriques pour correspondre à votre cas d'utilisation
-  }
+  }*/
 }
-
-
 
 // Fermeture des connections
 export function teardown(data) {
-  writer.close();
-  reader.close();
+  producerWriter.close();
+  producerReader.close();
+  consumerReader.close();
   connection.close();
 }
